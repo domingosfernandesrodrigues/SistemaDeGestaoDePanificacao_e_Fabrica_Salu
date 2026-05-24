504ef3559f0a30bca78b8c3321c3c807bc4d4566
Domingos Fernandes Rodrigues <domingosfernandesrodrigues89@gmail.com>
2026-05-24 00:24:09 -0400

    feat: adiciona migracao de sincronizacao de banco para garantir campos totalmente atualizados

---
+++ Backend/SGPF.Infrastructure/Migrations/20260524040403_SincronizarCamposFaltantes.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace SGPF.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class SincronizarCamposFaltantes : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
